96d6ad0f202d5f71201f3f22cc3315e9bdf536b2
Najeeb <najeebq@gmail.com>
2020-07-19 13:29:13 -0700

    Updates

---
+++ precache-manifest.e4aed57d379570224048114bee332fef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc548dbb1ae99b771b558dd06bf571b",
+    "url": "/covid-19tracker/index.html"
+  },
+  {
+    "revision": "f2f7673b8bee54f80a30",
+    "url": "/covid-19tracker/static/css/main.f3f57083.chunk.css"
+  },
+  {
+    "revision": "043e6eb3e81e9bf8ee86",
+    "url": "/covid-19tracker/static/js/2.34c8c8da.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19tracker/static/js/2.34c8c8da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f7673b8bee54f80a30",
+    "url": "/covid-19tracker/static/js/main.c62a7aec.chunk.js"
+  },
+  {
+    "revision": "a3adbf391b4e2533eae5",
+    "url": "/covid-19tracker/static/js/runtime-main.b6dba57a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19tracker/static/media/image.d7265326.png"
+  }
+]);
